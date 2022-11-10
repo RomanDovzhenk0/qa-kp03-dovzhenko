df44fb66332670d2f897c2587cba66ffe3778859
Roman Dovzhenko <dovzhenko.roman.kp03@gmail.com>
2022-11-10 16:38:28 +0200

    Created a prototype of the system

---
+++ main.py
@@ -0,0 +1,28 @@
+class FileSystem:
+
+    def mkdir(self, path):
+        pass
+
+    def delete_node(self, path):
+        pass
+
+    def ls(self, path):
+        pass
+
+    def move_node(self, old_path, new_path):
+        pass
+
+    def create_binary_file(self, file_path, content):
+        pass
+
+    def read_content_from_file(self, file_path):
+        pass
+
+    def create_file(self, file_path):
+        pass
+
+    def create_buffer_file(self, file_path):
+        pass
+
+    def add_content_to_file(self, file_path, content):
+        pass
